adb2fa2bcea023f76776df8bf234644dd77d019b
Nils Wildt <nilswildt@web.de>
2024-06-19 17:06:29 +0200

    update highlevel

--- src/APCE.jl
+++ src/APCE.jl
@@ -6,7 +6,7 @@ configdir(args...) = projectdir("configs", args...)
 outputdir(args...) = projectdir("output", args...)
 
 export run, normalization_functions, aPCE_MultivariatePolynomialDegrees, ∂∂I_aPCE_PsiPolynomialMatrix, RowVecs, ColVecs, GaussianCollocation, train!, predict, UQ, partitionTrainTest, aPCE, aPCE_OrthonormalBasis, create_Polynomial_Degrees, create_basis,
-	evaluate_Ψ, train!
+    evaluate_Ψ, train!
 
 include("APCEfunctions.jl")
 include("APCEderivatives.jl")
@@ -16,16 +16,16 @@ include("utils.jl")
 
 
 @setup_workload begin
-	FT = Float64
-	TrainingInput = rand(25, 2) |> Array{FT}
-	TrainingOutput = rand(25, 2) |> Array{FT} |> Array{FT}
-	@compile_workload begin
-		degree = 3
-		apc_instance = aPCE(TrainingInput, degree; outdim = size(TrainingOutput, 2), OrthonormalRepresentation = true, qnorm = 0.7, normalize_data = true)
-		train!(apc_instance, TrainingInput, TrainingOutput; bayesian_inversion = true, reg_order = 3)
-		predict(apc_instance, TrainingInput)
-		UQ(apc_instance)
-	end
+    FT = Float64
+    TrainingInput = rand(10, 2) |> Array{FT}
+    TrainingOutput = rand(10, 2) |> Array{FT} |> Array{FT}
+    @compile_workload begin
+        degree = 2
+        apc_instance = aPCE(TrainingInput, degree; outdim=size(TrainingOutput, 2), OrthonormalRepresentation=true, normalize_data=true)
+        train!(apc_instance, TrainingInput, TrainingOutput; bayesian_inversion=true, reg_order=3)
+        predict(apc_instance, TrainingInput)
+        UQ(apc_instance)
+    end
 end
 
 
